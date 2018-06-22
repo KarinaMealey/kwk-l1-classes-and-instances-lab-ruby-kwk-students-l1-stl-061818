3519933fdfa4c96d532e3648a1520d0f34ca2525
KarinaMealey <kem32002@gmail.com>
2018-06-22 17:42:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,3 +4,18 @@ class Dog
     @cute = cute
     @tail = tail
     @barks  = barks
+  end 
+  
+  def is_it_cute
+    @cute
+  end
+  
+  def tail_length
+    @tail
+  end 
+  
+  def bark
+    @bark
+  end 
+  
+  
